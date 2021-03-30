4e9186f2fc2e22c9c5427e387dc8b9c3c85d59c9
khaithumc <khaithumc@gmail.com>
2021-03-30 16:16:34 +0700

    Update Android advance 1 30.03.2021

--- AndroidBasic/app/src/main/java/com/example/androidbasic/FirstActivity.kt
+++ AndroidBasic/app/src/main/java/com/example/androidbasic/FirstActivity.kt
@@ -21,36 +21,6 @@ class FirstActivity : AppCompatActivity() {
            intent = Intent(this@FirstActivity, SecondActivity::class.java)
             startActivity(intent)
         })
-        Log.d(TAG,"onCreate")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
 }
